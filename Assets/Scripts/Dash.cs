507fdf12984bc7654e120779c69916a1b73743ce
Yanick <yanick.belanger@yahoo.com>
2020-02-01 00:20:54 -0500

    Updated dash system

--- Assets/Scripts/Dash.cs
+++ Assets/Scripts/Dash.cs
@@ -1,17 +1,27 @@
 ﻿using Assets.Scripts.Managers;
 using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Scripts
 {
+    public enum DashState
+    {
+        Ready,
+        Dashing,
+        Cooldown
+    }
+
     public class Dash : MonoBehaviour
     {
-        public const KeyCode DashKeyCode = KeyCode.LeftShift;
         public const float ForceModifier = 150.0f;
+        public const KeyCode DashKeyCode = KeyCode.LeftShift;
+        public const float DashCooldown = 2.0f;
 
         private LevelManager _levelManager = null;
-        private bool _wasTriggerKeyPressed = false;
+        private float _currentTimeUntilReady = 0.0f;
+        private DashState _currentDashState = DashState.Ready;
+
+        private GameObject _dashEffect = null;
 
         void Start()
         {
@@ -20,28 +30,58 @@ namespace Assets.Scripts
 
         void Update()
         {
-            this._wasTriggerKeyPressed = Input.GetKeyDown(Dash.DashKeyCode);
-        }
-
-        private void FixedUpdate()
-        {
-            if (!this._wasTriggerKeyPressed)
+            if (this._currentDashState == DashState.Cooldown || this._currentDashState == DashState.Dashing)
             {
                 return;
             }
 
-            this._isDashing = true;
-            Vector2 previousVelocity = this._levelManager.Player.rigidBody.velocity;
-            int playerDirectionX = Math.Sign(previousVelocity.x);
+            bool wasTriggerKeyPressed = Input.GetKeyDown(Dash.DashKeyCode);
 
-            if (playerDirectionX == 0 || playerDirectionX == 1)
+            if (!wasTriggerKeyPressed)
             {
-                this._levelManager.Player.rigidBody.AddForce(Vector2.right * Dash.ForceModifier, ForceMode2D.Impulse);
+                return;
             }
 
+            this._currentDashState = DashState.Dashing;
+        }
+
+        private void FixedUpdate()
+        {
+            // Current state is READY, return and wait for next input.
+            if (this._currentDashState == DashState.Ready)
+            {
+                return;
+            }
+
+            // Current state is DASHING, trigger dash.
+            else if (this._currentDashState == DashState.Dashing)
+            {
+                int playerDirectionX = Math.Sign(this._levelManager.Player.rigidBody.velocity.x);
+
+                if (playerDirectionX == 0 || playerDirectionX == 1)
+                {
+                    this._levelManager.Player.rigidBody.AddForce(Vector2.right * Dash.ForceModifier, ForceMode2D.Impulse);
+                }
+
+                else
+                {
+                    this._levelManager.Player.rigidBody.AddForce(Vector2.left * Dash.ForceModifier, ForceMode2D.Impulse); ;
+                }
+
+                this._currentTimeUntilReady = Dash.DashCooldown;
+                this._currentDashState = DashState.Cooldown;
+            }
+
+            // Current state is COOLDOWN, decrease timer and reset state if needed.
             else
             {
-                this._levelManager.Player.rigidBody.AddForce(Vector2.left * Dash.ForceModifier, ForceMode2D.Impulse); ;
+                this._currentTimeUntilReady -= Time.fixedDeltaTime;
+
+                if (this._currentTimeUntilReady <= 0.0f)
+                {
+                    this._currentTimeUntilReady = 0.0f;
+                    this._currentDashState = DashState.Ready;
+                }
             }
         }
     }
