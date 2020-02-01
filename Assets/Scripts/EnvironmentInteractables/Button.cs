d60693efb172c50722830959a2bd53df9e93e860
Guillaume <guillaume_proulx@outlook.com>
2020-02-01 00:26:32 -0500

    Ajout de fonctions boutton et flames

---
+++ Assets/Scripts/EnvironmentInteractables/Button.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Controllers
+{
+    public class Button : MonoBehaviour
+    {
+        #region Declarations --------------------------------------------------
+
+
+        public Sprite buttonOpened;
+        public Sprite buttonClosed;
+        public int buttonOrder;
+        public bool signal;
+
+        private SpriteRenderer _spriteRenderer;
+        private AudioSource _audio;
+
+        #endregion
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _audio = GetComponent<AudioSource>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        { }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                if (!signal)
+                {
+                    _audio.Play();
+                }           
+                _spriteRenderer.sprite = buttonClosed;
+                signal = true;
+            }
+        }
+
+        private void buttonReset()
+        {
+            _spriteRenderer.sprite = buttonOpened;
+            signal = false;
+        }
+    }
+}
