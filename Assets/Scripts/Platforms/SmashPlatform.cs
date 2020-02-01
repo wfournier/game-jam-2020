80bf390dfe753bb1d9556fc1e67e7d9390c1031f
Dat Phi Tran <datphitran@gmail.com>
2020-02-01 02:16:19 -0500

    Ajout Smash Platform

---
+++ Assets/Scripts/Platforms/SmashPlatform.cs
@@ -0,0 +1,38 @@
+﻿using Assets.Scripts.Managers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmashPlatform : MonoBehaviour
+{
+    private PlatformEffector2D effector;
+    private float actualWaitTime;
+    public float waitTime = 0.3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        effector = GetComponent<PlatformEffector2D>();
+        actualWaitTime = waitTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (InputManager.VerticalDir == InputManager.VerticalDirections.Down)
+        {
+            
+            if (actualWaitTime <= 0){
+                effector.rotationalOffset = 180f;
+                actualWaitTime = waitTime;
+            } else {
+                actualWaitTime -= Time.deltaTime;
+            }
+        }
+        if (InputManager.JumpButton)
+        {
+            effector.rotationalOffset = 0;
+        }
+
+    }
+}
