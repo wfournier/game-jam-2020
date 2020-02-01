eb1c0d05dac56986422a09b0ba3ec71b251ca582
Yanick <yanick.belanger@yahoo.com>
2020-01-31 23:13:54 -0500

    Kinda working dash (more like a teleport tbh)

---
+++ Assets/Scripts/Dash.cs
@@ -0,0 +1,48 @@
+﻿using Assets.Scripts.Managers;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Dash : MonoBehaviour
+    {
+        public const KeyCode DashKeyCode = KeyCode.LeftShift;
+        public const float ForceModifier = 150.0f;
+
+        private LevelManager _levelManager = null;
+        private bool _wasTriggerKeyPressed = false;
+
+        void Start()
+        {
+            this._levelManager = FindObjectOfType<LevelManager>();
+        }
+
+        void Update()
+        {
+            this._wasTriggerKeyPressed = Input.GetKeyDown(Dash.DashKeyCode);
+        }
+
+        private void FixedUpdate()
+        {
+            if (!this._wasTriggerKeyPressed)
+            {
+                return;
+            }
+
+            this._isDashing = true;
+            Vector2 previousVelocity = this._levelManager.Player.rigidBody.velocity;
+            int playerDirectionX = Math.Sign(previousVelocity.x);
+
+            if (playerDirectionX == 0 || playerDirectionX == 1)
+            {
+                this._levelManager.Player.rigidBody.AddForce(Vector2.right * Dash.ForceModifier, ForceMode2D.Impulse);
+            }
+
+            else
+            {
+                this._levelManager.Player.rigidBody.AddForce(Vector2.left * Dash.ForceModifier, ForceMode2D.Impulse); ;
+            }
+        }
+    }
+}
